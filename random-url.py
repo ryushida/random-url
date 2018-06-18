07f443a26818dff827e06e28caa91bbe55b0beb5
Ryuhei Shida <ryushidadev@gmail.com>
2018-06-18 10:59:39 +0900

    Store random url in string

--- random-url.py
+++ random-url.py
@@ -8,6 +8,9 @@ print ('[%s]' % ', '.join(map(str, content)))
 from random import randint
 print(randint(0, len(content)-1))
 
+url = content[randint(0, len(content)-1)]
+print(url)
+
 import webbrowser
 
 webbrowser.open('https://github.com/')
