af02b35891e5432f1a5094a490b9fd469b286738
Ryuhei Shida <ryushidadev@gmail.com>
2018-06-18 10:53:18 +0900

    Read urls into list and print list

--- random-url.py
+++ random-url.py
@@ -1,3 +1,10 @@
+with open('urls.txt') as f:
+    content = f.readlines()
+
+content = [x.strip() for x in content]
+
+print ('[%s]' % ', '.join(map(str, content)))
+
 import webbrowser
 
 webbrowser.open('https://github.com/')
