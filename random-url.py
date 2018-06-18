38a4199879c0ea9793bc7149eab075c0be3548c2
Ryuhei Shida <ryushidadev@gmail.com>
2018-06-18 10:56:56 +0900

    Print random int corresponding to item in list

--- random-url.py
+++ random-url.py
@@ -5,6 +5,9 @@ content = [x.strip() for x in content]
 
 print ('[%s]' % ', '.join(map(str, content)))
 
+from random import randint
+print(randint(0, len(content)-1))
+
 import webbrowser
 
 webbrowser.open('https://github.com/')
